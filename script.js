59ed2240a0904de16286f843fa8ac2efbed1542e
sandeepsamanth <98205088+sandeepsamanth@users.noreply.github.com>
2023-02-11 19:52:23 +0530

    Update script.js
    updating function

--- script.js
+++ script.js
@@ -8,20 +8,45 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+  arr.map((person,index)=>{
+    if(person.marks>50){
+             console.log(person.name)
+    }
+  })
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+    arr.forEach((person,index)=>{
+    if(person.marks>50){
+             console.log(person.name)
+    }
+  })
 }
 
 function addData() {
   //Write your code here, just console.log
+    const a={id:4,name:"susan",age:"20",marks:45};
+  arr.push(a);
+  console.log(arr);
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+   const b=[];
+  arr.map((person,index)=>{
+    if(person.marks>50){
+             b.push(person);
+    }
+  })
+  console.log(b);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+   const c=[ {id:4,name:"susan",age:"20",marks:45},
+            {id:5,name:"nalina",age:"20",marks:49},
+            {id:6,name:"gita",age:"20",marks:5}]
+    const d=[...arr,...c];
+    console.log(d);
 }
